35c85ca9c5fcc2bbbfd2f1bc30b0a7375bd5e83d
sc.lee <sclee9040@gmail.com>
2025-05-27 10:17:04 +0900

    vue.js 1차 환경 설정 완료

---
+++ client/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+<!-- client/src/components/HelloAPI.vue -->
+<template>
+  <div>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { message: '' };
+  },
+  mounted() {
+    fetch('http://localhost:5000/api/hello')
+      .then(res => res.json())
+      .then(data => { this.message = data.message; });
+  }
+}
+</script>
